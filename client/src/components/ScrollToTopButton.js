2953afdb154644ed104d3601fda64b50ce33f6b5
Yassine ELMERZOUKI <yassinemerzouki.ym@gmail.com>
2021-08-27 16:03:42 +0100

    update

--- client/src/components/ScrollToTopButton.js
+++ client/src/components/ScrollToTopButton.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import Icon from './Icon';
 
 const ScrollToTopButton = ()=>{
-
+    useEffect(()=>{
+        const toTop= document.querySelector(".scroll_top");
+        window.addEventListener("scroll",()=>{
+            if(window.pageYOffset >100){
+                toTop.classList.add("active");
+            }else{
+                toTop.classList.remove("active");
+            }
+        })
+    });
+        const handleClick=()=>{
+            window.scrollTo(0, 0);
+        }
     return(
-        <button className="scroll_top">
-            <div >
-                <Icon name='icon-arrow-up' size={35} color='white' />
-            </div>
+        <button className="scroll_top" onClick={handleClick}>
+                <Icon name='arrow-up' size={25}  />
         </button>
     )
 }
